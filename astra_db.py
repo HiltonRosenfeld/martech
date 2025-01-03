071c4c6d647ed840cd8a925a1fa09475ad834d44
HiltonRosenfeld <hilton.rosenfeld@datastax.com>
2025-01-03 17:23:56 +1100

    Initial commit

---
+++ astra_db.py
@@ -0,0 +1,25 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_SECURE_BUNDLE_PATH = os.getenv("ASTRA_DB_SECURE_BUNDLE_PATH")
+
+KEYSPACE = "martech"
+
+class AstraSession:
+    def __init__(self, keyspace=KEYSPACE, secure_bundle_path=ASTRA_DB_SECURE_BUNDLE_PATH, application_token=ASTRA_DB_APPLICATION_TOKEN):
+        cloud_config = {'secure_connect_bundle': secure_bundle_path}
+        auth_provider = PlainTextAuthProvider('token', application_token)
+        cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+        self.session = cluster.connect()
+        self.session.set_keyspace(keyspace)
+
+    def shutdown(self):
+        self.session.shutdown()
+    
+    
+
